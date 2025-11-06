17a649f87ee724c27c1314e42d47033456f12ef3
Antonio Dinglasan <antonioandres.dinglasan@benilde.edu.ph>
2025-11-06 08:30:05 +0800

    Added Registration Setup

--- TechFest.py
+++ TechFest.py
@@ -1,2 +1,13 @@
 # TechFest Registration System A Python program that records event participants and analyzes track diversity.
 
+
+# Task 1 Registration Setup
+print ('\nWelcome To SMIT TechFest!!')
+print ('\nEvent organized by Antonio Dinglasan of APPDAET BTCS1')
+
+num_participants = int(input('How many participants will register?'))
+if num_participants <= 0:
+    print('Invalid number of Participants')
+    exit()
+
+# Task 2 Collect Participant Information
