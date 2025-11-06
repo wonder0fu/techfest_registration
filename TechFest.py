661c8fc7b59d0d51ec7d8d59565eaafebabb079e
Antonio Dinglasan <antonioandres.dinglasan@benilde.edu.ph>
2025-11-06 09:40:48 +0800

    Added Duplicate Name Detection

--- TechFest.py
+++ TechFest.py
@@ -5,7 +5,7 @@
 print ('\nWelcome To SMIT TechFest!!')
 print ('\nEvent organized by Antonio Dinglasan of APPDAET BTCS1')
 
-num_participants = int(input('How many participants will register?'))
+num_participants = int(input('How many participants will register? '))
 if num_participants <= 0:
     print('Invalid number of Participants')
     exit()
@@ -19,5 +19,29 @@ for i in range(num_participants):
     participants.append ({"name": name, "track": track})
 
 print("Registered Participants:")
-for i, p in enumerate(participants, 1):
-    print(f"{i}. {p['name']} - {p['track']}")
+for i, j in enumerate(participants, 1):
+    print(f"{i}. {j['name']} - {j['track']}")
+
+# Task 3: Track Diversity Report
+unique_tracks = {p["track"] for p in participants}
+
+if len(unique_tracks) < 2:
+    print("\nNot enough variety in tracks.")
+else:
+    print("\nTracks offered in this event:", ", ".join(unique_tracks))
+
+# Task 4 Duplicate Name Detection
+
+duplicate_name = set ()
+duplicate_found = False
+
+for j in participants:
+    if j["name"] in duplicate_name:
+        print(f"Duplicate Name Found: {j['name']}")
+        duplicate_found = True
+        break
+    duplicate_name.add(j["name"])
+
+if not duplicate_found:
+    print ("No Duplicate Names Found")
+
